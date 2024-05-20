138b672c52363268c7a9f5a32aa4b14bd0b9c318
Eos <100361496+theycelz@users.noreply.github.com>
2024-05-19 22:25:33 -0400

    refactor(ContaBancaria): Tipagem adicionada, alteração na função sacar -> cobertura de testes

--- ContaBancaria.py
+++ ContaBancaria.py
@@ -1,19 +1,21 @@
 class ContaBancaria:
-    def __init__(self, numero, titular, saldo=0):
+    def __init__(self, numero:int, titular:str, saldo:float=0.00):
         self.saldo = saldo
         self.numero = numero
         self.titular = titular
 
-    def sacar(self, valor):
-        if valor > 0 and valor <= self.saldo:
-            self.saldo -= valor
-            return True
-        else:
+    def sacar(self, valor:float) -> bool:
+        if valor <= 0:
+            print("O valor de saque deve ser positivo.")
             return False
+        if valor > self.saldo:
+            print("Saldo insuficiente.")
+            return False
+        self.saldo -= valor
+        return True
 
-    def depositar(self, valor):
-        if valor > 0:
-            self.saldo += valor
+    def depositar(self, valor:float):
+        self.saldo += valor
 
     def visualizar_saldo(self):
         return self.saldo
@@ -21,12 +23,9 @@ class ContaBancaria:
     def extrato(self):
         return f"Titular: {self.titular}\nNúmero da conta: {self.numero}\nSaldo: {self.saldo}"
 
-    def transferir(self, destino, valor):
+    def transferir(self, destino, valor:float):
         if self.sacar(valor):
             destino.depositar(valor)
             print(f"Transferência de R${valor:.2f} para a conta {destino.numero} realizada com sucesso.")
         else:
             print("Transferência falhou devido a saldo insuficiente.")
-
-
-
