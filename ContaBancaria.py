05673a52435b30cd3539feca766b20f80c9947df
Eos <100361496+theycelz@users.noreply.github.com>
2024-05-19 22:32:38 -0400

    refactor(ContaBancaria): Tipagem extra de retorno adicionada, alteração de funções para cobertura de testes

--- ContaBancaria.py
+++ ContaBancaria.py
@@ -15,17 +15,23 @@ class ContaBancaria:
         return True
 
     def depositar(self, valor:float):
+        if valor <= 0:
+            print("O valor de depósito deve ser positivo.")
+            return
         self.saldo += valor
 
-    def visualizar_saldo(self):
+    def visualizar_saldo(self) -> float:
         return self.saldo
 
-    def extrato(self):
+    def extrato(self)->str:
         return f"Titular: {self.titular}\nNúmero da conta: {self.numero}\nSaldo: {self.saldo}"
 
-    def transferir(self, destino, valor:float):
+    def transferir(self, destino: 'ContaBancaria', valor:float) -> bool:
+        
         if self.sacar(valor):
             destino.depositar(valor)
             print(f"Transferência de R${valor:.2f} para a conta {destino.numero} realizada com sucesso.")
+            return True
         else:
             print("Transferência falhou devido a saldo insuficiente.")
+            return False
