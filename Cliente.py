4c0bbdfc586e687a3b05efcd2a6c0b955ec937d7
Eos <100361496+theycelz@users.noreply.github.com>
2024-05-22 20:20:10 -0400

    feat(Cliente.py, ContaBancaria.py): Método sacar() atualizado para POO e classe cliente expandida

--- Cliente.py
+++ Cliente.py
@@ -1,5 +1,18 @@
 class Cliente:
-    pass
+    def __init__(self, nome:str, _cpf:str, _endereco:str):
+        self.nome = nome
+        self._endereco = _endereco
+        self.contas = []
+    
+    def realizar_transacao(self, conta, transacao:str):
+        transacao.registrar(conta)
+    
+    def adicionar_conta(self, conta):
+        self.contas.append(conta)
+                           
 
 class PessoaFisica(Cliente):
-    pass
+    def __init__(self, nome:str, endereco:str, data_nascimento:str,cpf:str):
+        super().__init__(nome, endereco)
+        self._cpf = cpf
+        self.data_nascimento = data_nascimento
--- ContaBancaria.py
+++ ContaBancaria.py
@@ -1,9 +1,53 @@
+from Historico import Historico
 class ContaBancaria:
-    def __init__(self, numero:int, titular:str, saldo:float=0.00):
-        self.saldo = saldo
-        self.numero = numero
-        self.titular = titular
+    def __init__(self, _numero:int, _agencia:int, _titular:str, _saldo:float, _historico:Historico):
+        self._numero = _numero
+        self._agencia = "0001"
+        self._titular = _titular
+        self._saldo = 0.00
+        self._historico = Historico()
+    
+    @classmethod
+    def nova_conta(cls, numero:int, cliente:str, saldo:float=0.00):
+        return cls(numero, cliente, saldo)
+    
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def titular(self):
+        return self._titular
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+def sacar(self, valor: float) -> bool:
+    if valor <= 0:
+        print("O valor de saque deve ser positivo.")
+        return False
+    
+    elif valor > self._saldo:
+        print("Saldo insuficiente.")
+        return False
+    
+    else:
+        self._saldo -= valor
+        print(f"Saque de R${valor:.2f} realizado com sucesso.")
+        return True
 
+    
+
+'''
     def sacar(self, valor:float) -> bool:
         if valor <= 0:
             print("O valor de saque deve ser positivo.")
@@ -34,6 +78,6 @@ class ContaBancaria:
             return True
         else:
             print("Transferência falhou devido a saldo insuficiente.")
-            return False
+            return False '''
 class ContaCorrente:
     pass 
